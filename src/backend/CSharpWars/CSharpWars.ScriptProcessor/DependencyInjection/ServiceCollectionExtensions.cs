2ba771acb32881428ee37f795136b4bea2e5ca12
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2019-10-20 17:48:24 +0200

    Simplified processing middleware

--- src/backend/CSharpWars/CSharpWars.ScriptProcessor/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/backend/CSharpWars/CSharpWars.ScriptProcessor/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using CSharpWars.Common.DependencyInjection;
 using CSharpWars.Logic.DependencyInjection;
-using CSharpWars.ScriptProcessor.Interfaces;
 using CSharpWars.ScriptProcessor.Middleware;
 using CSharpWars.ScriptProcessor.Middleware.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,13 +12,10 @@ namespace CSharpWars.ScriptProcessor.DependencyInjection
         {
             serviceCollection.ConfigureCommon();
             serviceCollection.ConfigureLogic();
-            serviceCollection.AddSingleton<IBotScriptCache, BotScriptCache>();
             serviceCollection.AddScoped<IMiddleware, Middleware.Middleware>();
             serviceCollection.AddScoped<IPreprocessor, Preprocessor>();
             serviceCollection.AddScoped<IProcessor, Processor>();
             serviceCollection.AddScoped<IPostprocessor, Postprocessor>();
-            serviceCollection.AddScoped<IBotProcessingFactory, BotProcessingFactory>();
-            serviceCollection.AddScoped<IBotScriptCompiler, BotScriptCompiler>();
         }
     }
 }
--- src/backend/CSharpWars/CSharpWars.ScriptProcessor/Middleware/Processor.cs
+++ src/backend/CSharpWars/CSharpWars.ScriptProcessor/Middleware/Processor.cs
@@ -1,24 +1,104 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using CSharpWars.Common.Extensions;
+using CSharpWars.DtoModel;
+using CSharpWars.Enums;
 using CSharpWars.Logic.Interfaces;
-using CSharpWars.ScriptProcessor.Interfaces;
+using CSharpWars.Scripting;
+using CSharpWars.Scripting.Model;
 using CSharpWars.ScriptProcessor.Middleware.Interfaces;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
 
 namespace CSharpWars.ScriptProcessor.Middleware
 {
     public class Processor : IProcessor
     {
-        private readonly IBotProcessingFactory _botProcessingFactory;
+        private readonly ConcurrentDictionary<Guid, Script> _scriptCache = new ConcurrentDictionary<Guid, Script>();
 
-        public Processor(IBotProcessingFactory botProcessingFactory)
+        private readonly IBotLogic _botLogic;
+
+        public Processor(IBotLogic botLogic)
         {
-            _botProcessingFactory = botProcessingFactory;
+            _botLogic = botLogic;
         }
 
         public async Task Go(ProcessingContext context)
         {
-            var botProcessing = context.Bots.Select(bot => _botProcessingFactory.Process(bot, context));
+            var botProcessing = context.Bots.Select(bot => Process(bot, context));
             await Task.WhenAll(botProcessing);
+        }
+
+        public async Task Process(BotDto bot, ProcessingContext context)
+        {
+            var botProperties = context.GetBotProperties(bot.Id);
+            try
+            {
+                var botScript = await GetCompiledBotScript(bot);
+                var scriptGlobals = ScriptGlobals.Build(botProperties);
+                await botScript.RunAsync(scriptGlobals);
+            }
+            catch
+            {
+                botProperties.CurrentMove = PossibleMoves.ScriptError;
+            }
+        }
+
+        private async Task<Script> GetCompiledBotScript(BotDto bot)
+        {
+            if (!ScriptStored(bot.Id))
+            {
+                try
+                {
+                    var script = await _botLogic.GetBotScript(bot.Id);
+                    var botScript = Compile(script);
+                    StoreScript(bot.Id, botScript);
+                }
+                catch (Exception ex)
+                {
+                    //
+                }
+            }
+
+            return LoadScript(bot.Id);
+        }
+
+        public Script Compile(String script)
+        {
+            var decodedScript = script.Base64Decode();
+            var mscorlib = typeof(Object).Assembly;
+            var systemCore = typeof(Enumerable).Assembly;
+            var dynamic = typeof(DynamicAttribute).Assembly;
+            var csharpScript = typeof(BotProperties).Assembly;
+            var enums = typeof(PossibleMoves).Assembly;
+            var scriptOptions = ScriptOptions.Default.AddReferences(mscorlib, systemCore, dynamic, csharpScript, enums);
+            scriptOptions = scriptOptions.WithImports("System", "System.Linq", "System.Collections", "System.Collections.Generic", "CSharpWars.Enums", "CSharpWars.Scripting", "CSharpWars.Scripting.Model", "System.Runtime.CompilerServices");
+            var botScript = CSharpScript.Create(decodedScript, scriptOptions, typeof(ScriptGlobals));
+            botScript.WithOptions(botScript.Options.AddReferences(mscorlib, systemCore));
+            botScript.Compile();
+            return botScript;
+        }
+
+        public Boolean ScriptStored(Guid botId)
+        {
+            return _scriptCache.ContainsKey(botId);
+        }
+
+        public void StoreScript(Guid botId, Script script)
+        {
+            _scriptCache.AddOrUpdate(botId, script, (a, b) => script);
+        }
+
+        public Script LoadScript(Guid botId)
+        {
+            if (ScriptStored(botId))
+            {
+                return _scriptCache[botId];
+            }
+            throw new ArgumentException("No cached script for bot was found.", nameof(botId));
         }
     }
 }
